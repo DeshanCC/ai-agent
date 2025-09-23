0bf263f25b1855277a120c0d7c6502b40834b62f
DeshanCC <deshancharuka95@gmail.com>
2025-09-23 22:29:09 +1000

    Connect gemini api to generate text based answers

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+)
+print(response.text)
