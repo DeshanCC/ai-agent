520b792c9807b4779b9002a6266737f31bd0466c
DeshanCC <deshancharuka95@gmail.com>
2025-09-29 22:13:16 +1000

    Made prompt to take form the cmd

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("Please provide a prompt!!")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+        model='gemini-2.0-flash-001', contents=prompt
     )
     print(response.text)
     if response is None or response.usage_metadata is None:
-        print("Response is comp")
+        print("Response is not looking good :-(")
         return
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
