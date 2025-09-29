0b458741e4c436bb3db7248f845624fa5935ad90
DeshanCC <deshancharuka95@gmail.com>
2025-09-29 22:57:49 +1000

    Adding the verbose flag for debugging

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     if len(sys.argv) < 2:
         print("Please provide a prompt!!")
         sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
     prompt = sys.argv[1]
 
     messages = [
@@ -25,7 +28,9 @@ def main():
     if response is None or response.usage_metadata is None:
         print("Response is not looking good :-(")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose_flag:
+        print(f"User Prompt: {response.usage_metadata.prompt_token_count}")
+        print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
 
 main()
