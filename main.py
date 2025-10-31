6b324ca868884d02d321732e74f4c52b15c8d3a6
DeshanCC <deshancharuka95@gmail.com>
2025-11-01 07:46:35 +1100

    Register get_files_info function to the AI Agent

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
     if len(sys.argv) < 2:
         print("Please provide a prompt!!")
@@ -20,17 +31,31 @@ def main():
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+        config=config
     )
-    print(response.text)
+
     if response is None or response.usage_metadata is None:
         print("Response is not looking good :-(")
         return
-    if verbose_flag:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+    if response.function_calls:
+        for function_call in response.function_calls:
+            f"Calling function: {function_call.name}({function_call.args})"
+    else:
+        print(response.text)
+
 
 main()
